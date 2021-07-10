8efcefec374408ecf8d9f22abea00e1429c67f5c
Anant Bansal <anant19bansal@gmail.com>
2021-07-10 10:55:33 +0530

    add log in and sign up buttons and finish first page

--- lib/app_screens/firstPage.dart
+++ lib/app_screens/firstPage.dart
@@ -8,10 +8,39 @@ class FirstPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        child: Image(
-          // width: 500,
-          height: 60,
-          image: NetworkImage('https://www.pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distracting.png'),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 220, 0, 0),
+          child: Column(
+            children: [
+              Image(
+                // width: 500,
+                height: 60,
+                image: NetworkImage('https://www.pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distracting.png'),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 130, 0, 0),
+                child: ElevatedButton(
+                  onPressed: () {}, 
+                  style: ElevatedButton.styleFrom(primary: Colors.blue[400]),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 120, vertical: 12),
+                    child: Text('Log in'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                child: ElevatedButton(
+                  onPressed: () {}, 
+                  style: ElevatedButton.styleFrom(primary: Colors.blue[400]),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 116, vertical: 12),
+                    child: Text('Sign up'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
